f4ea780eafbecd462ac8eb254e08a810af996818
plang007 <plang760@hotmail.com>
2018-06-19 18:23:11 +0800

    fuck11111111111111

--- new.java
+++ new.java
@@ -4,14 +4,14 @@ public class class2quiz {
 //	Every even number greater than 2 can be expressed as the sum of two primes
 //  Check if this is true, starting from 4
 	public static void main(String arg[]) {
-		for (int i = 4; i<1; i+=2) {
+		for (int i = 4; i<111111111; i+=2) {
 			if (!twosumprime(i)) {
 				System.out.println("GoldBach is not always right");
-				System.out.println(i);
+				break;
 				System.out.println(i+1); // new
 			}
 		}
-		System.out.println("GoldBach is right for numbers under 10000");
+		System.out.println(" is right for numbers under 10000");
 	}
 	
 	public static boolean twosumprime(int even){
